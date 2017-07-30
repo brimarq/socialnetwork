b1e36d3b252e170da14667018436e5116296b97f
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-30 17:56:25 -0500

    Renamed main VC file and class

--- socialnetwork/SignInVC.swift
+++ socialnetwork/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  socialnetwork
 //
 //  Created by Brian Marquis on 7/30/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
