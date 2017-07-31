a80d914613049eada9ed09edf95e975c6c88c85a
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-31 16:52:37 -0500

    Installed FBSDKLoginKit Cocoapod; completed Facebook+Firebase Authentication

--- socialnetwork/SignInVC.swift
+++ socialnetwork/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,36 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookBtnTapped(_ sender: Any) {
+        
+        // Facebook authentication
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                //print("EKXXI: Unable to authenticate with Facebook - \(error)")
+                print("EKXXI: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("EKXXI: User cancelled Facebook authentication")
+            } else {
+                print("EKXXI: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    // Firebase authentication
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                //print("EKXXI: Unable to authenticate with Firebase - \(error)")
+                print("EKXXI: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("EKXXI: Successfully authenticated with Firebase")
+            }
+        
+        })
+    }
 
 }
 
