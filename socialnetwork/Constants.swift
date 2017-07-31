fa77d5ecd9ed6c3e917510cf20a587fc1cc80c11
Brian Marquis <rbmarquis.dev@gmail.com>
2017-07-31 13:29:03 -0500

    Sign-in custom classes completed

---
+++ socialnetwork/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  socialnetwork
+//
+//  Created by Brian Marquis on 7/31/17.
+//  Copyright © 2017 brimarq. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
