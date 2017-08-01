d9fe2d255d20c4ea9e75f2c4bb0e62d42b5b7d5a
Brian Marquis <rbmarquis.dev@gmail.com>
2017-08-01 14:52:59 -0500

    Completed feed tableview cell ui and connected iboutlets

--- socialnetwork/CircleView.swift
+++ socialnetwork/CircleView.swift
@@ -18,8 +18,7 @@ class CircleView: UIImageView {
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
     }
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
+    override func layoutSubviews() {
         layer.cornerRadius = self.frame.width / 2
     }
 
---
+++ socialnetwork/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  socialnetwork
+//
+//  Created by Brian Marquis on 8/1/17.
+//  Copyright © 2017 brimarq. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
